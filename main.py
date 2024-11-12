4e34a96d260ac3990e5bb30fbb333adc66be757b
Millerioux Maxime <milleriouxmaxime@gmail.com>
2024-11-12 22:20:37 +0100

    Added function to extract product

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
+
 
 def get_soup(url):
         # Envoyer une requête pour récupérer le contenu HTML de la page
@@ -10,8 +12,9 @@ def get_soup(url):
     soup = BeautifulSoup(html_content, 'html.parser')
     return soup
     
-def extract_html(url):
-    soup = get_soup(url)
+
+def extract_book(book_url):
+    soup = get_soup(book_url)
 
     table = soup.find_all("td")
     universal_product_code = table[0].string
@@ -33,7 +36,7 @@ def extract_html(url):
 
     image_url = f"https://books.toscrape.com{soup.img["src"][5:]}"
 
-    result = {"Product Page URL": url,
+    result = {"Product Page URL": book_url,
         "Universal Product Code": universal_product_code,
         "Title": title,
         "Price including tax": price_including_tax,
@@ -45,6 +48,7 @@ def extract_html(url):
         "Image URL": image_url}
     
     return result
+
 
 def extract_categories():
     soup = get_soup("https://books.toscrape.com/")
@@ -60,13 +64,50 @@ def extract_categories():
     
     return results
 
+
+def extract_products(category_url):
+    soup = get_soup(category_url)
+    section = soup.section
+    products_li = section.ol.find_all("li")
+    products = []
+    for product in products_li:
+        products.append(f"https://books.toscrape.com/catalogue{product.a["href"][8:]}")
+
+    return products
+        
+
 def main():
-    url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html" 
-    result = extract_html(url)
-    #print(result)
+
     categories = extract_categories()
-    print(categories)
-    
+    all_books = []
+    for category in categories[:5]:
+        products = extract_products(category)
+        for product in products[:5]:
+            all_books.append(extract_book(product))
+
+    fieldnames = all_books[0].keys()
+    with open('output.csv', mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(all_books)
+
+    folder_path = "images"
+    os.makedirs(folder_path, exist_ok=True)
+
+    for book in all_books:
+        url = book["Image URL"]
+        file_name = f"{book["Title"]}.jpg"
+        image_path = f"{folder_path}/{file_name}"
+
+        # Create the folder if it doesn't exist
+        
+
+        # Fetch the image and save it to the specified path
+        response = requests.get(url)
+        with open(image_path, "wb") as file:
+            file.write(response.content)
+
+
 
 if __name__ == "__main__":
     main()
